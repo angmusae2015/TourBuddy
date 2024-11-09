5027fa83e76186290151552f2bb211318ba565b1
angmusae2015 <angmusae2015@gmail.com>
2024-11-09 16:41:25 +0900

    MainActivity에서 LoginActivity로 전환하는 launcher를 추가함

--- App/app/src/main/java/com/tourbuddy/app/MainActivity.java
+++ App/app/src/main/java/com/tourbuddy/app/MainActivity.java
@@ -1,8 +1,14 @@
 package com.tourbuddy.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,10 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
+        ActivityResultLauncher<Intent> loginLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult o) {
+                    if (o.getResultCode() == RESULT_OK) {
+                        // TODO: 로그인 성공 후 실행할 코드
+                    }
+                }
+            });
+
         if (!isLoggedIn()) {
-            // TODO: 로그인 액티비티로 전환하는 코드
+            // 로그인 액티비티로 전환
+            loginLauncher.launch(new Intent(MainActivity.this, LoginActivity.class));
         }
 
+        // 로그인이 완료되면 앱 실행 화면으로 전환
         setContentView(R.layout.activity_main);
     }
 
