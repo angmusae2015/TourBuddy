b6bed56303bc72185502d3a7bc80e62d0d639766
hyeongjun22 <l5726493@naver.com>
2024-11-14 23:28:50 +0900

    11

--- App/app/src/main/java/com/tourbuddy/app/MainActivity.java
+++ App/app/src/main/java/com/tourbuddy/app/MainActivity.java
@@ -1,24 +1,34 @@
 package com.tourbuddy.app;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import com.tourbuddy.app.databinding.ActivityMainBinding;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // View Binding 설정
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // 버튼 클릭 리스너 설정
+        binding.loginButton.setOnClickListener(v -> {
+            // 버튼 내려가는 애니메이션 로드 및 적용
+            Animation moveDownAnim = AnimationUtils.loadAnimation(this, R.anim.move_down_animation);
+            binding.loginField.startAnimation(moveDownAnim);
+
+            // 이미지가 보이도록 설정하고 페이드 인 애니메이션 적용
+            binding.textField.setVisibility(View.VISIBLE); // 이미지 보이게 설정
+            Animation fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in_animation);
+            binding.textField.startAnimation(fadeInAnim);
         });
     }
 }
