cb83918cbb82a66e818a2cbe5a981e45d426f6aa
angmusae2015 <angmusae2015@gmail.com>
2024-11-16 16:36:11 +0900

    main.xml의 구성 요소들의 ID를 다른 요소들의 ID의 형식과 통일함 로그인이 완료되면 MainActivity에 홈 화면이 등장하도록 추가함 홈 탭을 선택했을 때 표시되는 HomeTabFragment를 추가함

---
+++ App/app/src/main/java/com/tourbuddy/app/HomeTabFragment.java
@@ -0,0 +1,45 @@
+package com.tourbuddy.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tourbuddy.app.databinding.HomeBinding;
+
+/**
+ * 홈 화면의 홈 탭의 컨텐츠를 표시하는 Fragment.
+ * home.xml에 레이아웃이 정의되어 있으며, 크게 두 부분으로 나뉨
+ * 1. 여행 대쉬보드(id: userTravelDashboard): 사용자의 여행까지의 디데이나, 여행 중인 지역의 날씨 등 필요한 정보를 간략하게 표시하는 영역.
+ *      TODO: 현재 레이아웃에서는 예시 사진이 바탕으로 들어가 있음. 사진을 배경으로 사용하면 위에 표시할 텍스트가 잘 보이지 않아 단색 영역으로 바꾸기로 함.
+ * 2. 컨텐츠 피드(id: contentsFeed): 다른 사용자들의 포스트들을 스크롤해서 볼 수 있는 영역. 포스트의 레이아웃은 post.xml에 정의되어 있음.
+ *      TODO: RecyclerView를 사용해서 스크롤을 구현할 예정.
+ *
+ */
+public class HomeTabFragment extends Fragment {
+    private HomeBinding binding;
+
+    public HomeTabFragment() {
+        // 반드시 비워둘 것
+    }
+
+    /**
+     * Fragment를 생성하는 Factory 메소드
+     * @return HomeTabFragment의 새 인스턴스
+     */
+    public static HomeTabFragment newInstance() {
+        HomeTabFragment fragment = new HomeTabFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = HomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
--- App/app/src/main/java/com/tourbuddy/app/MainActivity.java
+++ App/app/src/main/java/com/tourbuddy/app/MainActivity.java
@@ -4,22 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.tourbuddy.app.databinding.MainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser user;
+
+    private MainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityResultLauncher<Intent> loginLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                 o -> {
-                    if (o.getResultCode() == RESULT_OK) {
-                        setContentView(R.layout.main);
-                    }
+                    if (o.getResultCode() == RESULT_OK)
+                        setHome();
                 });
 
         // 현재 로그인한 유저가 없을 경우
         if (user == null) {
             // 로그인 액티비티로 전환
             loginLauncher.launch(new Intent(MainActivity.this, LoginActivity.class));
-        }
+        } else
+            setHome();
+    }
 
-        setContentView(R.layout.main);
+    /**
+     * 로그인이 완료되면 홈 화면으로 전환하고 각 탭의 화면을 표시하는 fragmentContainer에 홈 탭의 fragment를 채우는 메소드
+     */
+    private void setHome() {
+        binding = MainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragmentContainer, HomeTabFragment.newInstance())
+                .commit();
     }
 }
